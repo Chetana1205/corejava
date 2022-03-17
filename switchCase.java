d6fe25626555fdd525ed4355eff6b408c76fe0eb
Chetana1205 <101184515+Chetana1205@users.noreply.github.com>
2022-03-17 17:51:54 +0530

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,34 @@
+package ConditionStatements;
+
+public class switchCase {
+	public static void main(String[] args)
+	{
+		int n=1;
+		switch(1)
+		{
+		case 1:
+			System.out.println("one");
+		case 2:
+			System.out.println("two");
+		case 3:
+			System.out.println("three");
+		case 4:
+			System.out.println("four");
+		case 5:
+			System.out.println("five");
+		case 6:
+			System.out.println("six");
+		case 7:
+			System.out.println("Seven");
+		case 8:
+			System.out.println("eight");
+		case 9:
+			System.out.println("nine");
+		case 10:
+			System.out.println("ten");
+			break;
+		}
+		
+	}
+
+}
